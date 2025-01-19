791aebe945bf35eebcd976e4dedd6d7c3ed8b8f8
Daniil Zakharov <danilazakharov2003@gmail.com>
2025-01-19 16:41:47 +0300

    change test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -17,6 +17,9 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
+
 }
+
